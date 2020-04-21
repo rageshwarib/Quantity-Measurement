72c2024b7cf265cbca11c803df0206b497c3a9c2
rageshwari <ragu.bodke@gmail.com>
2020-04-21 12:55:55 +0530

    ADDED:Code to check equality of two feet Variables

---
+++ src/test/java/com/BridgeLabz/quantityMeasurement/QuantityTest.java
@@ -0,0 +1,13 @@
+package com.BridgeLabz.quantityMeasurement;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuantityTest {
+    @Test
+    public void given0FeetAnd0Feet_shouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1 , feet2);
+    }
+}
