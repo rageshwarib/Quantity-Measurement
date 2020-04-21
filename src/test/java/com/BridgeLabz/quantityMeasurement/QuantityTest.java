35791bbb9f0492e2ee4fb22dbe71c2eb4ca438f7
rageshwari <ragu.bodke@gmail.com>
2020-04-21 13:27:11 +0530

    ADDED:Code to check equality of feet and null values

--- src/test/java/com/BridgeLabz/quantityMeasurement/QuantityTest.java
+++ src/test/java/com/BridgeLabz/quantityMeasurement/QuantityTest.java
@@ -10,4 +10,16 @@ public class QuantityTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1 , feet2);
     }
+    @Test
+    public void given0FeetAnd1Feet_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1 , feet2);
+    }
+    @Test
+    public void given0FeetAndNull_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assert.assertNotEquals(feet1 , feet2);
+    }
 }
