d8c213b69095418ebcd1f4e16d8c85fcb08898d7
rageshwari <ragu.bodke@gmail.com>
2020-04-21 14:13:36 +0530

    ADDED:Code to check equality of two Inch variables with different types

---
+++ src/main/java/com/BridgeLabz/quantityMeasurement/Inch2.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz.quantityMeasurement;
+
+public class Inch2 {
+    private final double inchValue;
+
+    public Inch2(double value) {
+        this.inchValue = value;
+    }
+}
--- src/test/java/com/BridgeLabz/quantityMeasurement/QuantityTest.java
+++ src/test/java/com/BridgeLabz/quantityMeasurement/QuantityTest.java
@@ -58,4 +58,10 @@ public class QuantityTest {
         Inch inch2 = null;
         Assert.assertNotEquals(inch1 , inch2);
     }
+    @Test
+    public void givenTwoInchWithDifferentType_shouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch2 inch2 = new Inch2(0.0);
+        Assert.assertNotEquals(inch1 , inch2);
+    }
 }
